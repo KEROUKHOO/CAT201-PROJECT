4a7bf4e6d2db7dd7ecb29a87f121c1419463a411
Shao Doo <ahdoo.ling010519@gmail.com>
2022-01-03 17:27:03 +0800

    Update and add new features at income and expenses xml

--- app/src/main/java/com/example/expensemanager/Expenses.java
+++ app/src/main/java/com/example/expensemanager/Expenses.java
@@ -3,12 +3,40 @@ package com.example.expensemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
 
 public class Expenses extends AppCompatActivity {
+
+    TextInputLayout textInputLayout;
+    AutoCompleteTextView autoCompleteTextView;
+
+    ArrayList<String> expenses_category;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
+
+        textInputLayout = (TextInputLayout) findViewById(R.id.expenses_menu_drop);
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.expenses_drop_items);
+
+        expenses_category = new ArrayList<>();
+        expenses_category.add("Food");
+        expenses_category.add("Transport");
+
+        arrayAdapter = new ArrayAdapter<>(getApplication(), R.layout.list_item, expenses_category);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
+        autoCompleteTextView.setThreshold(1);
     }
 }
--- app/src/main/java/com/example/expensemanager/Income.java
+++ app/src/main/java/com/example/expensemanager/Income.java
@@ -3,12 +3,37 @@ package com.example.expensemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
 
 public class Income extends AppCompatActivity {
+
+    TextInputLayout textInputLayout;
+    AutoCompleteTextView autoCompleteTextView;
+
+    ArrayList<String> income_category;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
+
+        textInputLayout = (TextInputLayout) findViewById(R.id.income_menu_drop);
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.income_drop_items);
+
+        income_category = new ArrayList<>();
+        income_category.add("Salary");
+        income_category.add("Bonus");
+
+        arrayAdapter = new ArrayAdapter<>(getApplication(), R.layout.list_item, income_category);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
+        autoCompleteTextView.setThreshold(1);
+
     }
 }
